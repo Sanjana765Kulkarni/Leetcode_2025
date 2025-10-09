a7069e444a4749be3a60eb326ae9a1fef97fb8c1
Sanjana765Kulkarni <146453088+Sanjana765Kulkarni@users.noreply.github.com>
2025-10-09 09:40:09 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.cpp
@@ -0,0 +1,26 @@
+// Last updated: 09/10/2025, 09:40:07
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int start = 0, end = n - 1;
+        int ans = 0;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (citations[mid] >= n - mid)
+            {
+                ans = max(ans, n - mid);
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
