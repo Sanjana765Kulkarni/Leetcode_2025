4e316e50fdfec8e994be6ddeb7a0a14252337611
Sanjana765Kulkarni <146453088+Sanjana765Kulkarni@users.noreply.github.com>
2026-03-03 20:06:55 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,20 @@
+// Last updated: 03/03/2026, 20:06:54
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> pascal;
+        for ( size_t i =0; i<numRows; i++)
+        {
+            vector<int> row(i+1,1);
+            for(int j =1; j < i; j++)
+            {
+                row[j] = pascal[i-1][j] + pascal[i-1][j-1];
+            }
+            pascal.push_back(row);
+        }
+
+        return pascal;
+        
+    }
+};
