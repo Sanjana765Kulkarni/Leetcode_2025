96bc210dd416f423fdcda466ed23adbbbd104e95
Sanjana Kulkarni <146453088+Sanjana765Kulkarni@users.noreply.github.com>
2026-08-01 23:21:06 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -0,0 +1,28 @@
+// Last updated: 01/08/2026, 23:21:07
+class Solution {
+public:
+   vector<int> asteroidCollision(vector<int> &asteroids){
+    int n = asteroids.size();
+    vector<int> st;  
+    
+    for(int i=0; i < n; i++) {
+        if(asteroids[i] > 0) {
+            st.push_back(asteroids[i]);
+        }
+        else {
+            while(!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i])) {
+                st.pop_back();
+            }
+            
+            if(!st.empty() && st.back() == abs(asteroids[i])) {
+                st.pop_back();
+            }
+            else if(st.empty() || st.back() < 0){
+                st.push_back(asteroids[i]);
+            }
+        }
+    }
+    
+    return st;
+}
+};
